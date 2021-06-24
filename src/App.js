3680ebb5e74ebe7ae4e102772c6d9ca34e1363c5
rafaelwds <rafaelfernandoasilva@gmail.com>
2021-06-24 12:11:22 -0300

    First Commit todList

---
+++ src/App.js
@@ -0,0 +1,72 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [inText, setInText] = useState("");
+  const [itemList, setItemList] = useState([]);
+  const [editItem, setEditItem] = useState(null);
+
+  function onAddList() {
+    if (!inText) {
+      console.log("digite algo");
+    } else {
+      if (editItem) {
+        setItemList(
+          itemList.map((item) => {
+            if (item.id === editItem.id) {
+              return { id: editItem.id, inText };
+            }
+            return item;
+          })
+        );
+      } else {
+        setItemList([...itemList, { id: itemList.length + 1, inText }]);
+        setInText("");
+      }
+      setInText("");
+    }
+  }
+
+  function onRemove(item) {
+    console.log(item);
+    setItemList(itemList.filter((nameItem) => nameItem !== item));
+  }
+
+  function onEdit(item) {
+    console.log(item);
+  }
+
+  return (
+    <div className="App">
+      <h1>ToDo List</h1>
+      <div className="App-header">
+        <input
+          onChange={(e) => setInText(e.target.value)}
+          value={inText}
+          placeholder="digite aqui"
+        />
+        <button onClick={onAddList} style={{ marginLeft: 20 }}>
+          Adicionar
+        </button>
+        <br />
+        <ul>
+          {itemList?.length ? (
+            itemList.map((item) => (
+              <span key={item.id}>
+                <li className="li">
+                  {item.inText}{" "}
+                  <button onClick={() => onRemove(item)}>Remover</button>{" "}
+                  <button onClick={() => onEdit(item)}>Editar</button>
+                </li>
+              </span>
+            ))
+          ) : (
+            <p>Lista vázia</p>
+          )}
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default App;
